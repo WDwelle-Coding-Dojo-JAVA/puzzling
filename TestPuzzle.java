a6fc5d0ffd85afdaf78eb0339f41206b88b41c71
Will Dwelle <willdwelle@frontier.com>
2021-11-29 18:24:10 -0800

    initial

---
+++ TestPuzzle.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class TestPuzzle {
+    public static void main(String[] args) {
+        PuzzleUtil appTest = new PuzzleUtil();
+        Random randMachine = new Random();
+        System.out.println("random numbers: " + appTest.getTenRolls());
+    }
+}
