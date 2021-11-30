cd07d5bfc7eec3c049b4df6255eea86e83885ccb
Will Dwelle <willdwelle@frontier.com>
2021-11-29 19:04:33 -0800

    roll

--- PuzzleUtil.java
+++ PuzzleUtil.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 public class PuzzleUtil {
-    int getTenRolls(int[] roles){
+    int[] getTenRolls(int rolls){
         Random randMachine = new Random();
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = rd.nextInt();
+            arr[i] = randMachine.nextInt(20);
             System.out.println(arr[i]);
             }
+            return arr;
     }
 }
--- TestPuzzle.java
+++ TestPuzzle.java
@@ -2,7 +2,6 @@ import java.util.Random;
 public class TestPuzzle {
     public static void main(String[] args) {
         PuzzleUtil appTest = new PuzzleUtil();
-        Random randMachine = new Random();
-        System.out.println("random numbers: " + appTest.getTenRolls());
+        System.out.println("random numbers: " + appTest.getTenRolls(10));
     }
 }
