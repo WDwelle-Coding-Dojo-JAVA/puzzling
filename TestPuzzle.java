736d21739c960b0c2031ebaf0b56f3fa6890017d
Will Dwelle <willdwelle@frontier.com>
2021-11-29 19:22:57 -0800

    get letters

--- TestPuzzle.java
+++ TestPuzzle.java
@@ -3,5 +3,7 @@ public class TestPuzzle {
     public static void main(String[] args) {
         PuzzleUtil appTest = new PuzzleUtil();
         System.out.println("random numbers: " + appTest.getTenRolls(10));
+        System.out.println("random letter:" + appTest.getLetter(""));
+        System.out.println("random letter:" + appTest.getLetter(""*8));
     }
 }
